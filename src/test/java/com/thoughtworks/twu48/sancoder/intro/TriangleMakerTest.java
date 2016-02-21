068a1855c994a7e687abb8cc3573dfa808a0b246
SanCoder.Q <sancoder.q@gmail.com>
2016-02-21 14:10:30 +0800

    add hamcrest

---
+++ src/test/java/com/thoughtworks/twu48/sancoder/intro/TriangleMakerTest.java
@@ -0,0 +1,14 @@
+package com.thoughtworks.twu48.sancoder.intro;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+//Created by SanCoder on 2/21/16.
+public class TriangleMakerTest {
+
+    @Test
+    public void testMkAStar() throws Exception {
+
+    }
+}
